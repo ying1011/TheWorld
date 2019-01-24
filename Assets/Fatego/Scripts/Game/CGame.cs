0575c093a0c17bf4687787e1321d1d31babadd38
ying1011 <ying1011@hotmail.com>
2019-01-24 21:33:46 +0800

    en

---
+++ Assets/Fatego/Scripts/Game/CGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheWorld;
+
+public class CGame : MonoBehaviour {
+
+    CTheWorld GameCore;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameCore = CTheWorld.GetGameCore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameCore.Run();
+    }
+}
